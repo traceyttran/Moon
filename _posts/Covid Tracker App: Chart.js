ba399de5ca0aebba735dd38e022ec7e9206a12ac
traceyttran <36871511+traceyttran@users.noreply.github.com>
2020-12-28 13:28:09 -0800

    Delete Covid Tracker App: Chart.js

--- _posts/Covid Tracker App: Chart.js
+++
@@ -1,9 +0,0 @@
----
-layout: post
-title:  "First Post: Welcome to my Blog!"
-date:   2020-12-03
-excerpt: "
-I’ve created this site for many reasons - namely, to organize my proudest projects, to document the things I learn everyday and to have fun. I'm hoping this site will serve as a place to both..."
-tag:
-comments: false
----
