9a62900cd3d1b29fe1590787beb26518ab5c5a33
traceyttran <36871511+traceyttran@users.noreply.github.com>
2020-12-28 13:27:14 -0800

    Create Covid Tracker App: Chart.js

---
+++ _posts/Covid Tracker App: Chart.js
@@ -0,0 +1,9 @@
+---
+layout: post
+title:  "First Post: Welcome to my Blog!"
+date:   2020-12-03
+excerpt: "
+I’ve created this site for many reasons - namely, to organize my proudest projects, to document the things I learn everyday and to have fun. I'm hoping this site will serve as a place to both..."
+tag:
+comments: false
+---
